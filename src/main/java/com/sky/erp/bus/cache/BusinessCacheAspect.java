8eef6586450029a139a421a41cce39ceb28e28b6
sky <939382326@qq.com>
2020-07-16 18:58:56 +0800

    供应商管理的切面操作

--- src/main/java/com/sky/erp/bus/cache/BusinessCacheAspect.java
+++ src/main/java/com/sky/erp/bus/cache/BusinessCacheAspect.java
@@ -2,6 +2,7 @@ package com.sky.erp.bus.cache;
 
 import cn.hutool.log.Log;
 import com.sky.erp.bus.entity.Customer;
+import com.sky.erp.bus.entity.Provider;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -30,9 +31,30 @@ public class BusinessCacheAspect {
     private static final String POINT_CUSTOMER_REMOVE_BATCH = "execution(* com.sky.erp.bus.service.impl.CustomerServiceImpl.removeByIds(..))";
 
     /**
+     * 供应商切点表达式
+     */
+    private static final String POINT_PROVIDER_ADD = "execution(* com.sky.erp.bus.service.impl.ProviderServiceImpl.save(..))";
+    private static final String POINT_PROVIDER_UPDATE = "execution(* com.sky.erp.bus.service.impl.ProviderServiceImpl.updateById(..))";
+    private static final String POINT_PROVIDER_GET = "execution(* com.sky.erp.bus.service.impl.ProviderServiceImpl.getById(..))";
+    private static final String POINT_PROVIDER_REMOVE = "execution(* com.sky.erp.bus.service.impl.ProviderServiceImpl.removeById(..))";
+    private static final String POINT_PROVIDER_REMOVE_BATCH = "execution(* com.sky.erp.bus.service.impl.ProviderServiceImpl.removeByIds(..))";
+
+
+
+    /**
      * 客户前缀
      */
     private static final String CACHE_CUSTOMER_PREFIX = "customer";
+
+    /**
+     * 供应商前缀
+     */
+    private static final String CACHE_PROVIDER_PREFIX = "provider";
+    
+    
+    
+    
+//    ******************客户切面开始*************************************************************************************
 
     /**
      * 添加客户切入
@@ -110,4 +132,95 @@ public class BusinessCacheAspect {
             return customer;
         }
     }
+    
+//    **************客户切面结束*****************************************************************************************
+    
+    
+    
+    
+    
+//    ***************供应商切面开始***************************************************************************************
+
+    /**
+     * 添加供应商切入
+     */
+    @Around(POINT_PROVIDER_ADD)
+    public Object cacheProviderAdd(ProceedingJoinPoint joinPoint) throws Throwable {
+        Provider arg = (Provider)joinPoint.getArgs()[0];
+        Boolean flag = (Boolean) joinPoint.proceed();
+        if (flag){
+            cache.put(CACHE_PROVIDER_PREFIX+arg.getId(),arg);
+            log.info("以向缓存中添加供应商"+arg.getId());
+        }
+        return flag;
+    }
+
+    /**
+     * 修改供应商切入
+     */
+    @Around(POINT_PROVIDER_UPDATE)
+    public Object cacheProviderUpdate(ProceedingJoinPoint joinPoint) throws Throwable {
+        Provider arg = (Provider)joinPoint.getArgs()[0];
+        Boolean flag = (Boolean)joinPoint.proceed();
+        if (flag){
+            cache.put(CACHE_PROVIDER_PREFIX+arg.getId(),arg);
+            log.info("已更新缓存中供应商"+arg.getId());
+        }
+        return flag;
+    }
+
+    /**
+     * 删除供应商切入
+     */
+    @Around(POINT_PROVIDER_REMOVE)
+    public Object cacheProviderDelete(ProceedingJoinPoint joinPoint) throws Throwable {
+        Integer arg = (Integer)joinPoint.getArgs()[0];
+        Boolean flag = (Boolean)joinPoint.proceed();
+        if (flag){
+            if (cache.containsKey(CACHE_PROVIDER_PREFIX+arg)){
+                cache.remove(CACHE_PROVIDER_PREFIX+arg);
+                log.info("已删除数据库和缓存中供应商"+arg);
+            }
+            log.info("缓存中没有，只删除数据库中供应商"+arg);
+        }
+        return flag;
+    }
+
+    /**
+     * 批量删除供应商切入
+     */
+    @Around(POINT_PROVIDER_REMOVE_BATCH)
+    public Object cacheProviderDeleteBatch(ProceedingJoinPoint joinPoint) throws Throwable {
+        List<Integer> arg = (List<Integer>) joinPoint.getArgs()[0];
+        Boolean flag = (Boolean)joinPoint.proceed();
+        if (flag){
+            for (Integer id : arg) {
+                cache.remove(CACHE_PROVIDER_PREFIX+id);
+            }
+            log.info("已删除缓存中多个供应商"+arg.toString());
+        }
+        return flag;
+    }
+
+    /**
+     * 获取供应商切入
+     */
+    @Around(POINT_PROVIDER_GET)
+    public Object cacheProviderGet(ProceedingJoinPoint joinPoint) throws Throwable {
+        Integer arg = (Integer)joinPoint.getArgs()[0];
+        if (cache.containsKey(CACHE_PROVIDER_PREFIX+arg)){
+            log.info("从缓存中获取供应商"+arg);
+            return cache.get(CACHE_PROVIDER_PREFIX+arg);
+        } else {
+            Provider provider = (Provider)joinPoint.proceed();
+            log.info("从数据库中获取供应商"+arg);
+            return provider;
+        }
+    }
+    
+    
+//    ***************供应商切面结束***************************************************************************************
+    
+    
+    
 }
