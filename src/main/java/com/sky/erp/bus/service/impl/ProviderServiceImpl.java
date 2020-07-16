f8e16973cd6a73f8c6da9686b1c9f46763097b81
sky <939382326@qq.com>
2020-07-16 19:03:12 +0800

    ProviderServiceImpl中重写crud方法

--- src/main/java/com/sky/erp/bus/service/impl/ProviderServiceImpl.java
+++ src/main/java/com/sky/erp/bus/service/impl/ProviderServiceImpl.java
@@ -6,6 +6,9 @@ import com.sky.erp.bus.service.IProviderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
+import java.io.Serializable;
+import java.util.Collection;
+
 /**
  * <p>
  *  服务实现类
@@ -16,5 +19,30 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ProviderServiceImpl extends ServiceImpl<ProviderMapper, Provider> implements IProviderService {
+
+    @Override
+    public boolean save(Provider entity) {
+        return super.save(entity);
+    }
+
+    @Override
+    public Provider getById(Serializable id) {
+        return super.getById(id);
+    }
+
+    @Override
+    public boolean removeById(Serializable id) {
+        return super.removeById(id);
+    }
+
+    @Override
+    public boolean removeByIds(Collection<? extends Serializable> idList) {
+        return super.removeByIds(idList);
+    }
+
+    @Override
+    public boolean updateById(Provider entity) {
+        return super.updateById(entity);
+    }
 
 }
