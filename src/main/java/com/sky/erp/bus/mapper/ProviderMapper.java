6c4860fb6c6ccf0001afb0c86267cdcec2e063cf
sky <939382326@qq.com>
2020-07-16 18:24:51 +0800

    生产供应商各层代码

---
+++ src/main/java/com/sky/erp/bus/mapper/ProviderMapper.java
@@ -0,0 +1,16 @@
+package com.sky.erp.bus.mapper;
+
+import com.sky.erp.bus.entity.Provider;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author syk
+ * @since 2020-07-16
+ */
+public interface ProviderMapper extends BaseMapper<Provider> {
+
+}
---
+++ src/main/java/com/sky/erp/bus/service/impl/ProviderServiceImpl.java
@@ -0,0 +1,20 @@
+package com.sky.erp.bus.service.impl;
+
+import com.sky.erp.bus.entity.Provider;
+import com.sky.erp.bus.mapper.ProviderMapper;
+import com.sky.erp.bus.service.IProviderService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author syk
+ * @since 2020-07-16
+ */
+@Service
+public class ProviderServiceImpl extends ServiceImpl<ProviderMapper, Provider> implements IProviderService {
+
+}
