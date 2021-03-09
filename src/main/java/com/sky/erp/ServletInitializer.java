37eb9d768162fc61faf99c42cfac778e87541299
syk-kk <939382326@qq.com>
2021-03-09 09:25:18 +0800

    解释ServletInitializer类的作用

--- src/main/java/com/sky/erp/ServletInitializer.java
+++ src/main/java/com/sky/erp/ServletInitializer.java
@@ -3,6 +3,9 @@ package com.sky.erp;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * 该类和方法的作用是可以让项目使用外部的tomcat，可以把项目打包后放到外部tomcat中运行
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
